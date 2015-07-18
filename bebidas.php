05966110bb0ac875477029facf845f2340e18b96
Enjoyproyect <yesidrondon@outlook.com>
2015-07-18 11:40:42 -0500

    version 1.0.3

--- bebidas.php
+++ bebidas.php
@@ -8,13 +8,49 @@
 		<meta name="description" content="" />
 		<meta name="keywords" content="" />
 		<meta name="author" content="" />
-		<link rel="shortcut icon" href="../favicon.ico">
-		<link rel="stylesheet" type="text/css" href="../css/menu.css" />
-		<script src="../js/modernizr.custom.js"></script>
-		<link href="//netdna.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.css" rel="stylesheet"/>
+		<link rel="shortcut icon" href="favicon.ico">
+		<link rel="stylesheet" type="text/css" href="css/menu.css" />
+		<script src="js/modernizr.custom.js"></script>
+		<link href="css/font-awesome.css" rel="stylesheet"/>
 	</head>
 	<body class="cbp-spmenu-push">
-		<?php include './menu.php';?>
+				<nav class="cbp-spmenu cbp-spmenu-vertical cbp-spmenu-left" id="cbp-spmenu-s1">
+			<a href="index.html" id="indice">Indice</a>
+			<a href="entradas.html">Entradas</a>
+			<a href="menu-infantil-cremas.html">Ceviches / Menú infantil / Cremas</a>
+			<a href="ceviches.html">Ceviches</a>
+			<a href="ensaladas.html">Ensaladas</a>
+			<a href="platos-ligeros.html">Platos ligeros</a>
+			<a href="pescados.html">Pescados</a>
+			<a href="arroces.html">Arroces</a>
+			<a href="carnes.html">Carnes</a>
+			<a href="pastas.html">Pastas</a>
+			<a href="salsas-adicionales.html">Salsas y Adicionales</a>
+			<a href="bebidas.html">Bebidas</a>
+			<a href="vinos.html">Vinos</a>
+			<a href="parfaits-pasteleria-fria.html">Postres y Pastelería</a>
+			<a href="cafe.html">Café</a>
+			<div class="ubicacion">
+				<p><span class="fa fa-phone-square"></span> Domicilios Gourmet:	(7) 64 374 61 - 300 560 2336
+				<br /><span class="fa fa-map-marker"></span> Dirección: Cra 35 No. 48 - 84, Bucaramanga, Colombia
+				<br /><span class="fa fa-envelope-o"></span> Email: contacto@tavologourmet.com</p>
+			</div>			
+		</nav>
+		<header>
+			<div class="container" style="height:100% float:left;">
+				<div class="main">
+					<section class="buttonset">
+						<button id="showLeftPush"><i class="fa fa-bars"></i><span class="ocul"> Menú</span></button>
+					</section>
+				</div>
+				<div class="title">
+					<img src="images/logo.png" title="Restaurante Bucaramanga - Tavolo gourmet" alt="Restaurante Bucaramanga - Tavolo gourmet">
+				</div>
+				<div style="display:inline-block; text-align:right; width:10%; font-size:0.8em; vertical-align:middle; height:100%">
+					<a href="http://tavologourmet.com" class="boton"><i class="fa fa-undo"></i><span class="ocul"> Volver</span></a>
+				</div>
+			</div>
+		</header>
 		<section class="info">
 				<div class="adicionales">
 					<div class="Title">
